c7abbccb5d60c4b38e49912452ce95784fd06dcf
Roberto Grijalva <45749929+Gr1l3m@users.noreply.github.com>
2021-09-09 14:45:41 -0600

    test

--- install.sh
+++ install.sh
@@ -14,11 +14,11 @@ sudo dpkg -i python3.8-minimal_3.8.0-3~18.04.1_arm64.deb
 echo "lib stdlib"
 wget http://ports.ubuntu.com/pool/universe/p/python3.8/libpython3.8-stdlib_3.8.0-3~18.04.1_arm64.deb
 sudo dpkg -i libpython3.8-stdlib_3.8.0-3~18.04.1_arm64.deb
+'
 
 echo "python"
 wget http://ports.ubuntu.com/pool/universe/p/python3.8/python3.8_3.8.0-3~18.04.1_arm64.deb
 sudo dpkg -i python3.8_3.8.0-3~18.04.1_arm64.deb
-'
 
 echo "lib python"
 wget http://ports.ubuntu.com/pool/universe/p/python3.8/libpython3.8_3.8.0-3~18.04.1_arm64.deb
