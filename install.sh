130534615aa503ae0e5a7a0a9bd62d8793ec8ba5
Roberto Grijalva <45749929+Gr1l3m@users.noreply.github.com>
2022-01-18 13:40:15 -0600

    Update install.sh
    New name for files in ports.ubuntu.com (ubuntu1 was added) and new release version: 18.04.2 arm64

--- install.sh
+++ install.sh
@@ -3,31 +3,31 @@
 echo “Python 3.8 dev installer”
 
 echo "1- lib minimal"
-wget http://ports.ubuntu.com/pool/universe/p/python3.8/libpython3.8-minimal_3.8.0-3~18.04.1_arm64.deb
-sudo dpkg -i libpython3.8-minimal_3.8.0-3~18.04.1_arm64.deb
+wget http://ports.ubuntu.com/pool/universe/p/python3.8/libpython3.8-minimal_3.8.0-3ubuntu1~18.04.2_arm64.deb
+sudo dpkg -i libpython3.8-minimal_3.8.0-3ubuntu1~18.04.2_arm64.deb
 
 echo "2- python minimal"
-wget http://ports.ubuntu.com/pool/universe/p/python3.8/python3.8-minimal_3.8.0-3~18.04.1_arm64.deb
-sudo dpkg -i python3.8-minimal_3.8.0-3~18.04.1_arm64.deb
+wget http://ports.ubuntu.com/pool/universe/p/python3.8/python3.8-minimal_3.8.0-3ubuntu1~18.04.2_arm64.deb
+sudo dpkg -i python3.8-minimal_3.8.0-3ubuntu1~18.04.2_arm64.deb
 
 echo "3- lib stdlib"
-wget http://ports.ubuntu.com/pool/universe/p/python3.8/libpython3.8-stdlib_3.8.0-3~18.04.1_arm64.deb
-sudo dpkg -i libpython3.8-stdlib_3.8.0-3~18.04.1_arm64.deb
+wget http://ports.ubuntu.com/pool/universe/p/python3.8/libpython3.8-stdlib_3.8.0-3ubuntu1~18.04.2_arm64.deb
+sudo dpkg -i libpython3.8-stdlib_3.8.0-3ubuntu1~18.04.2_arm64.deb
 
 echo "4- python"
-wget http://ports.ubuntu.com/pool/universe/p/python3.8/python3.8_3.8.0-3~18.04.1_arm64.deb
-sudo dpkg -i python3.8_3.8.0-3~18.04.1_arm64.deb
+wget http://ports.ubuntu.com/pool/universe/p/python3.8/python3.8_3.8.0-3ubuntu1~18.04.2_arm64.deb
+sudo dpkg -i python3.8_3.8.0-3ubuntu1~18.04.2_arm64.deb
 
 echo "5- lib python"
-wget http://ports.ubuntu.com/pool/universe/p/python3.8/libpython3.8_3.8.0-3~18.04.1_arm64.deb
-sudo dpkg -i libpython3.8_3.8.0-3~18.04.1_arm64.deb
+wget http://ports.ubuntu.com/pool/universe/p/python3.8/libpython3.8_3.8.0-3ubuntu1~18.04.2_arm64.deb
+sudo dpkg -i libpython3.8_3.8.0-3ubuntu1~18.04.2_arm64.deb
 
 echo "6- lib dev"
-wget http://ports.ubuntu.com/pool/universe/p/python3.8/libpython3.8-dev_3.8.0-3~18.04.1_arm64.deb
-sudo dpkg -i libpython3.8-dev_3.8.0-3~18.04.1_arm64.deb
+wget http://ports.ubuntu.com/pool/universe/p/python3.8/libpython3.8-dev_3.8.0-3ubuntu1~18.04.2_arm64.deb
+sudo dpkg -i libpython3.8-dev_3.8.0-3ubuntu1~18.04.2_arm64.deb
 
 echo "7- python dev"
-wget http://ports.ubuntu.com/pool/universe/p/python3.8/python3.8-dev_3.8.0-3~18.04.1_arm64.deb
-sudo dpkg -i python3.8-dev_3.8.0-3~18.04.1_arm64.deb
+wget http://ports.ubuntu.com/pool/universe/p/python3.8/python3.8-dev_3.8.0-3ubuntu1~18.04.2_arm64.deb
+sudo dpkg -i python3.8-dev_3.8.0-3ubuntu1~18.04.2_arm64.deb
 
 echo "OK! Python3.8 has been installed"
